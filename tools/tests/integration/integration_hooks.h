189916f270ff339c100add063dd878519189c632
aikoschurmann <aiko.schurmann@gmail.com>
2026-06-10 22:43:17 +0200

    test: suppress unused parameter warnings in integration hooks

--- tools/tests/integration/integration_hooks.h
+++ tools/tests/integration/integration_hooks.h
@@ -4,8 +4,10 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdint.h>
+#include <stddef.h>
 
 static bool test_hook(const void *val, void *err_ptr) {
+    (void)err_ptr;
     const char *s = (const char *)val;
     if (s && strcmp(s, "hook-fail") == 0) {
         return false;
@@ -14,6 +16,7 @@ static bool test_hook(const void *val, void *err_ptr) {
 }
 
 static bool test_array_hook(const void *val, void *err_ptr) {
+    (void)err_ptr;
     struct { void *data; size_t count; } *arr = (void*)val;
     if (arr->count != 3) {
         return false;
@@ -22,6 +25,7 @@ static bool test_array_hook(const void *val, void *err_ptr) {
 }
 
 static bool test_nested_hook(const void *val, void *err_ptr) {
+    (void)err_ptr;
     int64_t id = *(int64_t*)val;
     if (id == 0) {
         return false;
